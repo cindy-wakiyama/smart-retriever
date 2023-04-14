e55441e6ffaeb02da25d33e13df2963b5fa8ced0
ttruj <ttruj@tamu.edu>
2023-04-13 20:54:01 -0500

    Added more tests for third motor

--- motors.py
+++ motors.py
@@ -5,27 +5,39 @@
 
 """Simple test for using adafruit_motorkit with a stepper motor"""
 import time
-import board
+#import board
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper # this adds more controls to the onestep() function
 
 # connects to the hat
-kit = MotorKit(i2c=board.I2C())
+#kit = MotorKit(i2c=board.I2C())
+
+# connects to the first hat using the base address 0x60
+kit1 = MotorKit()
+
+# connects to the second hat using next address
+kit2 = MotorKit(address=0x61)
 
 # we can test motors one at a time
 print("tests starting")
 
-# Motor 1 connects to terminals M1 and M2 on the hat
+# Motor 1 connects to terminals M1 and M2 on the first hat
 print("testing stepper motor 1")
 for i in range(100):
     # onestep() is one single coil step for the motor
-    kit.stepper1.onestep()
+    kit1.stepper1.onestep()
     time.sleep(0.01)
 
-# Motor 2 connects to the terminals M3 and M4 on the hat
+# Motor 2 connects to the terminals M3 and M4 on the first hat
 print("testing stepper motor 2")
 for i in range(100):
-    kit.stepper2.onestep()
+    kit1.stepper2.onestep()
+    time.sleep(0.01)
+
+# Motor 3 connects to the terminals M1 and M2 on the second hat
+print("testing stepper motor 3")
+for i in range(100):
+    kit2.stepper1.onestep()
     time.sleep(0.01)
 
 
@@ -41,42 +53,64 @@ for i in range(100):
 
 print("testing stepper motor 1 with backward motion")
 for i in range(100):
-    kit.stepper1.onestep(direction=stepper.BACKWARD)
+    kit1.stepper1.onestep(direction=stepper.BACKWARD)
     time.sleep(0.01)
 
 print("testing stepper motor 2 with backward motion")
 for i in range(100):
-    kit.stepper2.onestep(direction=stepper.BACKWARD)
+    kit1.stepper2.onestep(direction=stepper.BACKWARD)
     time.sleep(0.01)
+
+print("testing stepper motor 3 with backward motion")
+for i in range(100):
+    kit2.stepper1.onestep(direction=stepper.BACKWARD)
+    time.sleep(0.01)
+
 
 print("testing stepper motor 1 with double coil motion")
 for i in range(100):
-    kit.stepper1.onestep(style=stepper.DOUBLE)
+    kit1.stepper1.onestep(style=stepper.DOUBLE)
     time.sleep(0.01)
 
 print("testing stepper motor 2 with double coil motion")
 for i in range(100):
-    kit.stepper2.onestep(style=stepper.DOUBLE)
+    kit1.stepper2.onestep(style=stepper.DOUBLE)
     time.sleep(0.01)
+
+print("testing stepper motor 3 with double coil motion")
+for i in range(100):
+    kit2.stepper1.onestep(style=stepper.DOUBLE)
+    time.sleep(0.01)
+
 
 print("testing stepper motor 1 with interleaving motion")
 for i in range(100):
-    kit.stepper1.onestep(style=stepper.INTERLEAVE)
+    kit1.stepper1.onestep(style=stepper.INTERLEAVE)
     time.sleep(0.01)
 
 print("testing stepper motor 2 with interleaving motion")
 for i in range(100):
-    kit.stepper2.onestep(style=stepper.INTERLEAVE)
+    kit1.stepper2.onestep(style=stepper.INTERLEAVE)
+    time.sleep(0.01)
+
+print("testing stepper motor 3 with interleaving motion")
+for i in range(100):
+    kit2.stepper1.onestep(style=stepper.INTERLEAVE)
     time.sleep(0.01)
 
 print("testing stepper motor 1 with microstep motion")
 for i in range(100):
-    kit.stepper1.onestep(style=stepper.MICROSTEP)
+    kit1.stepper1.onestep(style=stepper.MICROSTEP)
     time.sleep(0.01)
 
 print("testing stepper motor 2 with microstep motion")
 for i in range(100):
-    kit.stepper2.onestep(style=stepper.MICROSTEP)
+    kit1.stepper2.onestep(style=stepper.MICROSTEP)
+    time.sleep(0.01)
+
+print("testing stepper motor 3 with microstep motion")
+for i in range(100):
+    kit2.stepper1.onestep(style=stepper.MICROSTEP)
     time.sleep(0.01)
 
 print("tests finished")
