35f44e51dde34ede47e785d22490e24745bf4720
Cindy Wakiyama <cindywakiyama@gmail.com>
2023-04-13 21:07:12 -0500

    filled out add item function

--- handleRequest.py
+++ handleRequest.py
@@ -1,3 +1,48 @@
-#to house functions handling user requests
-import json
+import yaml
+
+# json file will store inventory
+# menu
+# add item
+# remove item
+# retrieve item
+# return item
+
+#open inventory file
+file = open("inventory.yml", "r+")
+inventory = yaml.safe_load(file)
+
+#menu options
+def add_item():
+    print("What item do you want to add to the inventory?")
+    item = "- '" + input() + "'"
+    print(item)
+    item_yaml = yaml.safe_load(item)
+    yaml.dump(item_yaml, file)
+  
+def remove_item():
+    print("remove item")
+
+def retrieve_item():
+    print("retrieve item")
+
+def return_item():
+    print("return item")
+
+def menu():
+    print("Choose an option: \n 1. Add item \n 2. Remove item \n 3. Retrive item \n 4. Return item \n")
+    choice = input()
+    if choice == "1":
+        add_item()
+    elif choice == "2":
+        remove_item()
+    elif choice == "3":
+        retrieve_item()
+    elif choice == "4":
+        return_item()
+    else:
+        menu()
+
+menu()
+
+
 
